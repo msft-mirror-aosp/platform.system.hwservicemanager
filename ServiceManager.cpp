7831df4b818de67e602ec9dfe38d0d0003ec2e1d
Steven Moreland <smoreland@google.com>
2024-12-07 00:28:29 +0000

    abort on pre 4.4 pidcon path
    setRequestingSid was added since kernel version 4.4, so
    we should no longer need the fallback.
    
    Bug: 376122465
    Test: boot
    Change-Id: I97c3c4dcd5aaebb33b2673fd0284a681511ec8b4

--- ServiceManager.cpp
+++ ServiceManager.cpp
@@ -47,6 +47,9 @@ AccessControl::CallingContext getBinderCallingContext() {
             android_errorWriteLog(0x534e4554, "121035042");
         }
 
+        CHECK_EQ(nullptr, self->getServingStackPointer())
+                << "Pid " << pid << " missing service context.";
+
         return AccessControl::getCallingContext(pid);
     } else {
         return { true, sid, pid };
